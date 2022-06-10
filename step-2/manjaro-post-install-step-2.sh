93be378a23da21f315815c000bf5ffd1f3cb9013
N0Raitor <n0raitor@pm.me>
2022-06-10 16:15:59 +0200

    Fixes

--- step-2/manjaro-post-install-step-2.sh
+++ step-2/manjaro-post-install-step-2.sh
@@ -11,13 +11,13 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg -
 # Set NumKey Enabled
 gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
 
-pacman_packages=( brave-browser yay nautilus-terminal arc-gtk-theme arc-icon-theme cantarell-fonts ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-roboto ttf-roboto-mono aspell aspell-de aspell-en hyphen hyphen-de hyphen-en neofetch speech-dispatcher unarchiver most system-config-printer tree atom audacity bleachbit brasero calibre ghidra gimp gimp-help-de gnome-builder handbrake ghc cabal-install haskell-language-server inkscape ipython kdenlive libreoffice-still libreoffice-still-de hunspell-de hunspell-en_us mythes-en mythes-de languagetool libmythes nitroshare obs-studio xreader gnome-keyring qbittorrent steam-manjaro glances virtualbox code vlc evolution )
+pacman_packages=( telegram-desktop tlp tlp-rdw brave-browser gnome-games lutris dia vim vim-spell-de vim-spell-en ghex darktable scribus openshot appimagelauncher yay nautilus-terminal arc-gtk-theme arc-icon-theme cantarell-fonts ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-roboto ttf-roboto-mono aspell aspell-de aspell-en hyphen hyphen-de hyphen-en neofetch speech-dispatcher unarchiver most system-config-printer tree atom audacity bleachbit brasero calibre ghidra gimp gimp-help-de gnome-builder handbrake ghc cabal-install haskell-language-server inkscape ipython kdenlive libreoffice-still libreoffice-still-de hunspell-de hunspell-en_us mythes-en mythes-de languagetool libmythes nitroshare obs-studio xreader gnome-keyring qbittorrent steam-manjaro glances virtualbox code vlc evolution )
 
-aur_packages=( 1password stacer-bin marktext-bin archlinux-artwork arch-logo-dark-wallpapers arch-linux-2d-wallpapers ttf-ms-fonts ttf-hackgen brother-dcpj315w balena-etcher bootstrap-studio edb-debugger-git github-desktop-bin gitkraken guitar-pro ida-free jdownloader2 jetbrains-toolbox kali-undercover kazam libreoffice-extension-languagetool maltego protonmail-bridge-bin rambox-bin remarkable slack-desktop spotify synology-drive synology-note-station nvidia-system-monitor-git virtualbox-ext-oracle yed zoom minecraft-launcher multimc-bin)
+aur_packages=( 1password preload itch-bin zotero-bin stacer-bin marktext-bin archlinux-artwork arch-logo-dark-wallpapers arch-linux-2d-wallpapers ttf-ms-fonts ttf-hackgen brother-dcpj315w balena-etcher bootstrap-studio edb-debugger-git github-desktop-bin gitkraken guitar-pro ida-free jdownloader2 jetbrains-toolbox kali-undercover kazam libreoffice-extension-languagetool maltego protonmail-bridge-bin rambox-bin remarkable slack-desktop spotify synology-drive synology-note-station nvidia-system-monitor-git virtualbox-ext-oracle yed zoom minecraft-launcher multimc-bin)
 
-snap_packages=( remmina )  # optional: flat-remix flat-remix-gtk
+snap_packages=( remmina pomotroid )  # optional: flat-remix flat-remix-gtk
 
-flatpak_packages=(  )
+flatpak_packages=( "flathub org.signal.Signal" "flathub com.usebottles.bottles" )
 
 logfile=step-2.log
 
@@ -109,7 +109,7 @@ echo "### Installing Flatpak Packages ###"
 for package in "${flatpak_packages[@]}"				  
 do
 	echo -n "Installing $package "
-	flatpak install $package &>> $logfile
+	flatpak -y install $package &>> $logfile
 	echo "[OK]"
 done
 
@@ -123,6 +123,11 @@ echo -n "Installing flat-remix-gnome "
 pamac install --no-confirm balena-etcher &>> $logfile
 echo "[OK]"
 
+echo -n "Starting Battery Saving Service "
+sudo systemctl enable tlp &>> $logfile
+sudo systemctl start tlp &>> $logfile
+echo "[OK]"
 
 echo "ALL DONE"
+echo "If Balena-Etcher is not installed, feel free to open the appimage in the resource folder"
 echo "Now continue with Step 3"
--- step-3/manjaro-post-install-step-3.md
+++ step-3/manjaro-post-install-step-3.md
@@ -11,11 +11,20 @@ Final Steps
 * Enable AUR, SNAP, Flatpak in Pamac and take your Settings changes
 
 ## SSH-Key
+
 ```bash
 ssh-keygen -t rsa -b 4096 -o -a 100
 ```
 
+## most
+
+```bash
+# Insert into .bashrc file:
+export PAGER=most
+```
+
 ## Reduce Swappiness
+
 Forces System to use as much RAM as possible and reduces hard drive access
 
 ```bash
@@ -28,6 +37,7 @@ cat /proc/sys/vm/swappiness  # should be 10 now
 ```
 
 ## Trim SSD
+
 Enable Trim for SSD - optimize performance of ssd-drive
 
 ```bash
@@ -39,15 +49,19 @@ sudo systemctl status fstrim.timer  # should be active now
 ```
 
 ## Remove unuseful Applications
+
 Go to Pamac and remove apps
 
 ## Open Extensions
+
 ... And Enable or Install Extensions
 
 ## (optional) Clean Your System and Check for Errors
+
 See on my wiki: [https://n0raitor.com/archlinux](https://n0raitor.com/archlinux)
 
 ## Done
+
 This is the End of the "Manjaro Post Installer" steps. Feel free to add Issues, if some errors or bug occured or if you have any suggestions
 
 **Appearance**: Feel free to change the appearance e.g. using *flat-remix* of [https://drasite.com/](https://drasite.com/)
